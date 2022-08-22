87448da9fdccfedb3e25cfe890be577b48198ac5
satsu <satsu@DESKTOP-KGRC5OJ>
2022-08-21 23:40:55 -0300

    Add project files.

---
+++ Projectplusplus3/aqui2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+
+
+int main() {
+
+	float numeroDiv = 5.0 / 2;
+	printf("%f", numeroDiv);
+
+	float numeroDiv2 = 5 / 2;
+	printf("\n%f ", numeroDiv2);
+
+
+	return 0;
+}
